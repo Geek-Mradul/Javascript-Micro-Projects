180c2ba50ab8773e7765941725609a851e750c7b
Mradul Purohit <mradulpurohit123@gmail.com>
2024-10-06 15:24:14 +0530

    new

---
+++ Random-Password-Generator/script.js
@@ -0,0 +1,22 @@
+const passwordBox = document.getElementById("password");
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+<>?:";
+const allChar = upperCase + lowerCase + number + symbol;
+const lenght = 12;
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    while (lenght > password.length) {
+        password += allChar[Math.floor(Math.random() * allChar.length)];
+    }
+    passwordBox.value = password;
+}
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy")
+}
